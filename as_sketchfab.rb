d25d3eded0d1d83fab2df4f30d2da98ad08e15dd
Alexander C. Schreyer <mail@alexschreyer.net>
2013-07-15 13:52:26 -0400

    1.6 Changes: Temp folder change

--- as_sketchfab.rb
+++ as_sketchfab.rb
@@ -6,6 +6,6 @@ require 'extensions.rb'
 as_sketchfab = SketchupExtension.new "Sketchfab Uploader", "as_sketchfab/as_sketchfab_uploader.rb"
 as_sketchfab.copyright= 'Copyright 2012 Alexander C. Schreyer'
 as_sketchfab.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
-as_sketchfab.version = '1.5'
+as_sketchfab.version = '1.6'
 as_sketchfab.description = "A plugin to upload the current model to the Sketchfab website."
 Sketchup.register_extension as_sketchfab, true

2e9b39337384831bbeab8d74617b157b107095ef
Alexander C. Schreyer <mail@alexschreyer.net>
2014-02-18 15:21:34 -0500

    Version 1.7 changes
    New upload system for SU 2014 and other fixes

--- as_sketchfab.rb
+++ as_sketchfab.rb
@@ -1,11 +1,11 @@
-# Loader for as_plugins/as_sketchfab/as_sketchfab_uploader.rb
+# Loader for as_sketchfab/as_sketchfab_uploader.rb
 
-require 'sketchup.rb'
-require 'extensions.rb'
+require 'sketchup'
+require 'extensions'
 
 as_sketchfab = SketchupExtension.new "Sketchfab Uploader", "as_sketchfab/as_sketchfab_uploader.rb"
-as_sketchfab.copyright= 'Copyright 2012 Alexander C. Schreyer'
+as_sketchfab.copyright= 'Copyright 2012-2014 Alexander C. Schreyer'
 as_sketchfab.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
-as_sketchfab.version = '1.6'
+as_sketchfab.version = '1.7'
 as_sketchfab.description = "A plugin to upload the current model to the Sketchfab website."
 Sketchup.register_extension as_sketchfab, true
--- as_sketchfab/as_sketchfab_uploader.rb
+++ as_sketchfab/as_sketchfab_uploader.rb
@@ -1,5 +1,5 @@
 =begin
-Copyright 2012, Alexander C. Schreyer
+Copyright 2012-2014, Alexander C. Schreyer
 All Rights Reserved
 
 THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, 
@@ -9,11 +9,12 @@ FITNESS FOR A PARTICULAR PURPOSE.
 License:        GPL (http://www.gnu.org/licenses/gpl.html)
 
 Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
-Website:        http://www.alexschreyer.net/projects/
+
+Website:        http://www.alexschreyer.net/projects/sketchfab-uploader-plugin-for-sketchup/
 
 Name :          Sketchfab Uploader
-Version:        1.6
-Date :          7/15/2013
+Version:        1.7
+Date :          2/18/2014
 
 Description :   This plugin uploads the currently open model to Sketchfab.com
 
@@ -39,64 +40,95 @@ History:        1.0 (7/13/2012):
                 - Reorganized folders
                 1.6 (7/15/2013):
                 - Changed temp location to user folder to remove permission problem
+                1.7 (2/18/2014):
+                - Fixed temp location cleanup
+                - Removed thumbnail image export
+                  SU 2014 only:
+                - Added new upload method
+                - Implemented multipart upload via new API
+                - Added option to open model after uploading
 
 Issues:
-                - The post_url function does not accept returned data.
+                - For versions before SU 2014: the post_url function does not accept returned data.
+                
+Licenses:
+
+multipart-post
+==============
+
+https://github.com/nicksieger/multipart-post
+
+Copyright (c) 2007-2013 Nick Sieger nick@nicksieger.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the 'Software'), to deal in the Software without 
+restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom 
+the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 =end
 
+
+# ========================
+
+
 require 'sketchup'
+
+
+# ========================
+    
 
 module AS_SketchfabUploader
 
-    def self.show_dialog
+    # Some general variables
+
+    # Set temporary folder locations and filenames
+    # Don't use root or plugin folders because of writing permissions
+    # Get user directory for temporary file storage
+    @user_dir = (ENV['USERPROFILE'] != nil) ? ENV['USERPROFILE'] :
+                ( (ENV['HOME'] != nil) ? ENV['HOME'] : 
+                File.dirname(__FILE__) )
+    # Cleanup slashes
+    @user_dir = @user_dir.tr("\\","/")
+    @filename = File.join(@user_dir , 'temp_export.kmz')
     
-        # Get currently open model
-        mod = Sketchup.active_model 
+    # Exporter options - doesn't work with KMZ export, though
+    @options_hash = {  :triangulated_faces   => true,
+                      :doublesided_faces    => true,
+                      :edges                => true,
+                      :materials_by_layer   => false,
+                      :author_attribution   => true,
+                      :texture_maps         => true,
+                      :selectionset_only    => false,
+                      :preserve_instancing  => true }    
+    
+    
+    # ========================    
+   
 
-        # Set temporary filenames
-        # Don't use root or plugin folders because of writing permissions
-        # Get user directory for temporary file storage
-        @user_dir = (ENV['USERPROFILE'] != nil) ? ENV['USERPROFILE'] :
-                    ( (ENV['HOME'] != nil) ? ENV['HOME'] : 
-                    File.dirname(__FILE__) )
-        filename = File.join(@user_dir , 'temp_export.kmz')
-        i_filename = File.join(@user_dir , 'temp_export.png')
-
-        # Exporter options
-        options_hash = {  :triangulated_faces   => true,
-                          :doublesided_faces    => true,
-                          :edges                => true,
-                          :materials_by_layer   => false,
-                          :author_attribution   => true,
-                          :texture_maps         => true,
-                          :selectionset_only    => false,
-                          :preserve_instancing  => true }
+    def self.show_dialog_2013
+    # This uses a json approach to upload (for < SU 2014)
         
         # Export model as KMZ
-        if mod.export filename, options_hash then
+        if Sketchup.active_model.export @filename, @options_hash then
             
             # Open file as binary and encode it as Base64
-            contents = open(filename, "rb") {|io| io.read }
+            contents = open(@filename, "rb") {|io| io.read }
             encdata = [contents].pack('m')
             
-            # Image exporter keys
-            keys = {    :filename => i_filename,
-                        :width => 448,
-                        :height => 280,
-                        :antialias => true,
-                        :compression => 0.5,
-                        :transparent => false  } 
-                        
-            # Export thumbnail image and encode it
-            view = mod.active_view  
-            view.write_image keys
-            i_contents = open(i_filename, "rb") {|io| io.read }
-            i_encdata = [i_contents].pack('m')
-            
             # Then delete the temporary files
-            File.delete filename
-            File.delete i_filename
+            # File.delete @filename
             
             # Set up and show Webdialog
             dlg = UI::WebDialog.new('Sketchfab Uploader', false,'SketchfabUploader', 450, 520, 150, 150, true)
@@ -136,20 +168,13 @@ module AS_SketchfabUploader
                 
                 # Assemble JSON string
                 json = '{"contents":"' + encdata.split(/[\r\n]+/).join('\r\n') + '","filename":"temp.zip","title":"' + mytitle + '","description":"' + description + '","tags":"' + tags + '","token":"' + p + '","source":"sketchup-version"' + privString + '}'
-                # UI.messagebox json
                 
                 # Submit data to Sketchfab - need to use old API with JSON
                 d.post_url("https://api.sketchfab.com/model", json)
                 
-                # New API attempt - doesn't work this way:
-                # json = '{"fileModel":"' + encdata.split(/[\r\n]+/).join('\r\n') + '","filenameModel":"temp.zip","title":"' + mytitle + '","description":"' + description + '","tags":"' + tags + '","token":"' + p + '","source":"sketchup-version"' + privString + '}'                
-                # d.post_url("https://api.sketchfab.com/v1/models", json)
-                
                 defaults = Sketchup.write_default "Sketchfab", "api_token", p
                 d.execute_script('submitted()')
-                # Open Sketchfab Dashboard after submitting - disabled for now:
-                # sleep(3)
-                # UI.openURL "http://www.sketchfab.com/dashboard/"
+                
             }
     
             dlg_html = %Q~
@@ -179,7 +204,7 @@ module AS_SketchfabUploader
                 <p><label for="token">Your API token *</label><input type="text" name="token" id="token" value="" style="width:200px;" /></p>
                 <p><input type="submit" id="submit" value="Submit Model" style="font-weight:bold;" /></p>
             </form>
-            <p><span style="float:left;"><button value="Cancel" id="cancel">Dismiss</button></span><span style="float:right;margin-top:10px;">&copy; 2013 by <a href="http://www.alexschreyer.net/" title="http://www.alexschreyer.net/" target="_blank" style="color:orange">Alex Schreyer</a></span></p>
+            <p><span style="float:left;"><button value="Cancel" id="cancel">Dismiss</button></span><span style="float:right;margin-top:10px;">&copy; 2012-2014 by <a href="http://www.alexschreyer.net/" title="http://www.alexschreyer.net/" target="_blank" style="color:orange">Alex Schreyer</a></span></p>
             <p></p>
             <script type="text/javascript">
             $(function(){
@@ -237,18 +262,233 @@ module AS_SketchfabUploader
             
         else
         
-            UI.messagebox "Couldn't export model as " + filename
+            UI.messagebox "Couldn't export model as " + @filename
             
         end # if image converts
     
-    end # show_dialog
+    end # show_dialog_2013
+    
+    
+    # ========================
+
+    
+    def self.show_dialog_2014
+    # This uses the Ruby NET StdLibs instead of json
+
+
+        # Load Net and multipart post libraries
+        $: << File.dirname(__FILE__)+'/lib'
+        require 'net/http'
+        require 'json'
+        require 'net/http/post/multipart'
+        
+
+        # Set up and show Webdialog
+        dlg = UI::WebDialog.new('Sketchfab Uploader', false,'SketchfabUploader', 450, 520, 150, 150, true)
+        dlg.navigation_buttons_enabled = false
+        dlg.min_width = 450
+        dlg.max_width = 450
+        dlg.set_size(450,650)
+        logo = File.join(File.dirname(__FILE__) , 'uploader-logo.png')
+        
+        
+        # Close dialog callback
+        dlg.add_action_callback('close_me') {|d, p|    
+        
+            d.close
+            
+        }
+        
+        
+        # Callback to prefill page elements (token)
+        dlg.add_action_callback('prefill') {|d, p|   
+        
+            # Need to do this because we need to wait until page has loaded
+            mytoken = Sketchup.read_default "as_Sketchfab", "api_token", "Paste your token here"
+            c = "$('#token').val('" + mytoken + "')"
+            d.execute_script(c)
+            
+        }
+        
+        
+        # Callback to prepare and send model
+        dlg.add_action_callback('send') {|d, p|   
+        
+            # Get data from webdialog and clean it up a bit
+            # Token is p
+            description = d.get_element_value("description").gsub(/"/, "'")
+            mytitle = d.get_element_value("mytitle").gsub(/"/, "'")
+            tags = d.get_element_value("tags").gsub(/"/, "'")
+            tags.gsub!(/,*\s+/,' ')
+            private = d.get_element_value("private").gsub(/"/, "'")
+            password = d.get_element_value("password").gsub(/"/, "'")
+            
+            # Export model as KMZ and process
+            if Sketchup.active_model.export @filename, @options_hash then
+                
+                # Some feedback while we wait
+                d.execute_script("submitted()")               
+                
+                # Open file for multipart upload
+                encdata = UploadIO.new(File.new(@filename), "application/zip", "model.kmz")
+                
+                # Then delete the temporary file
+                # File.delete @filename
+            
+                # Submission URL
+                url = URI.parse('http://api.sketchfab.com/v1/models')
+                
+                # Prepare data for submission
+                req = Net::HTTP::Post::Multipart.new url.path,
+                          'token' => p,
+                          'fileModel' => encdata,
+                          'title' => mytitle,
+                          'description' => description,
+                          'tags' => tags,
+                          'private' => private,
+                          'password' => password,
+                          'source' => 'sketchup-version'
+                
+                # And submit it
+                res = Net::HTTP.start(url.host, url.port) do |http|
+                  http.request(req)
+                end
+                json = JSON.parse(res.body.gsub(/"/,"\""))
+                
+                @success = json['success']
+                if @success then 
+                
+                    # Get model info from result
+                    @model_id = json['result']['id']
+                    
+                    d.close
+                    # Give option to open uploaded model
+                    result = UI.messagebox 'Open Sketchfab model in default browser?', MB_YESNO
+                    UI.openURL "https://sketchfab.com/show/#{@model_id}" if result == 6  
+                    
+                else
+                
+                    d.close
+                    UI.messagebox "Sketchfab upload failed. Error: " + json['error']
+                
+                end
+                
+                # Save token for the next time
+                defaults = Sketchup.write_default "as_Sketchfab", "api_token", p                
+                
+            else 
+            
+                d.close
+                UI.messagebox "Couldn't export model as " + @filename
+            
+            end
+
+        }
+        
+        
+        # Set dialog HTML
+        dlg_html = %Q~
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml"><head><title>Sketchfab.com Uploader</title>
+        <style type="text/css">
+            * {font-family: Arial, Helvetica, sans-serif; font-size:13px;}
+            body {background-color:#3d3d3d;padding:10px;min-width:220px;}
+            h1, label, p {color:#eee; font-weight: bold;}
+            h1 {font-size:2em;color:orange}
+            a, a:hover, a:visited {color:orange}
+            input, button, textarea {color:#fff; background-color:#666; border:none;}
+            label {display: block; width: 150px;float: left;}
+        </style>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        </head>
+        <body>
+        <img src="~ + logo + %Q~" style="width:100%;" />
+        <p id="text">This dialog uploads the currently open model to Sketchfab.com. All fields marked with a * are mandatory.
+        You can get your API token from the <a href='http://sketchfab.com' title='http://sketchfab.com' target='_blank'>Sketchfab website</a> after registering there.</p>
+        <form id="SketchfabSubmit" name="SketchfabSubmit" action="">
+            <p><label for="mytitle">Model title *</label><input type="text" id="mytitle" name="mytitle" style="width:200px;" /></p>
+            <p><label for="description">Description</label><textarea name="description" id="description" style="height:3em;width:200px;"></textarea></p>
+            <p><label for="tags">Tags (space-separated)</label><input type="text" id="tags" name="tags" value="sketchup" style="width:200px;" /></p>
+            <p><label for="private">Make model private?</label><input type="checkbox" name="private" id="private" value="" /> <span style="font-weight:normal;">(PRO account required)</span></p>
+            <p id="pw-field" style="display:none;"><label for="password">Password</label><input type="text" name="password" id="password" value="" style="width:200px;" /></p>
+            <p><label for="token">Your API token *</label><input type="text" name="token" id="token" value="" style="width:200px;" /></p>
+            <p><input type="submit" id="submit" value="Submit Model" style="font-weight:bold;" /></p>
+        </form>
+        <p><span style="float:left;"><button value="Cancel" id="cancel">Dismiss</button></span><span style="float:right;margin-top:10px;">&copy; 2012-2014 by <a href="http://www.alexschreyer.net/" title="http://www.alexschreyer.net/" target="_blank" style="color:orange">Alex Schreyer</a></span></p>
+        <p></p>
+        <script type="text/javascript">
+        $(function(){
+          $("#SketchfabSubmit").submit(function(event){
+                event.preventDefault();
+                
+                if ($('#mytitle').val().length == 0) {
+                    alert('You must fill in a title.');
+                    return false;
+                }
+
+                if ($('#token').val().length < 32) {
+                    alert('Your token looks like it is too short. Please double-check.');
+                    return false;
+                }
+                
+                // Submit form and give feedback
+                token = $('#token').val();
+                window.location='skp:send@'+token;
+          });
+        });
+        $('#cancel').click(function(){
+            window.location='skp:close_me'; 
+        });
+        
+        $('#private').click(function(){
+            if ($(this).val() == 'True') {
+                $(this).val('');
+            } else {
+                $(this).val('True');
+            };
+            $('#pw-field').toggle(); 
+        });
+        
+        $(document).ready(function() {
+            window.location='skp:prefill';
+        });
+        
+        function submitted() {
+            $('h1').html('Processing...');
+            scomment = "Your model has been submitted. Please hang on while we wait for a response from Sketchfab.";
+            $('#text').html(scomment);
+            $('form').html('');                  
+        };        
+        
+        </script>
+        </body></html>
+        ~ # End of HTML
+        dlg.set_html(dlg_html)
+        dlg.show_modal    
+        
+    
+    end # show_dialog_2014
+    
+    
+    # ========================    
+    
     
 end # module
+
+
+# ========================
+
 
 # Create menu items
 unless file_loaded?(__FILE__)
 
-    UI.menu("File").add_item("Upload to Sketchfab") {AS_SketchfabUploader::show_dialog}
-    file_loaded(__FILE__)
+  # Pick based on version
+  if Sketchup.version.to_f < 14 then
+    UI.menu("File").add_item("Upload to Sketchfab") {AS_SketchfabUploader::show_dialog_2013} 
+  else
+    UI.menu("File").add_item("Upload to Sketchfab") {AS_SketchfabUploader::show_dialog_2014} 
+  end
+  
+  file_loaded(__FILE__)
  
 end

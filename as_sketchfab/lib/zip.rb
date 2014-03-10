0535a56a048e5ab9dd75a945d30cc0db8edbe509
Alexander C. Schreyer <mail@alexschreyer.net>
2014-03-09 22:27:47 -0400

    Fixed problems with the ZIP module

--- as_sketchfab/lib/zip.rb
+++ as_sketchfab/lib/zip.rb
@@ -1,16 +1,29 @@
 module Zip
 
-    # Have to check for old and newer Windows name
-	USING_WINDOWS = ( RUBY_PLATFORM =~/mswin/ or RUBY_PLATFORM =~/mingw/ )
-	if USING_WINDOWS
-		SevenZip = File.join(File.dirname(__FILE__), '..', 'bin', '7za.exe')
-	end
-
 	def self.create(zip_name, *files)
-		if USING_WINDOWS
-			system "#{SevenZip} a #{zip_name} #{files.join(' ')}"
+    
+        # Are we on Windows?
+		if ((/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil)
+        
+            # Prepare 7-Zip file location
+            sevenZip = File.join(File.dirname(__FILE__), '7za.exe')
+            sevenZip.gsub!(/\\/,'/')
+            sevenZip.gsub!('/lib','/bin')
+            
+            # Assemble Win system command
+            all_files =  files.join("\" \"")  
+            command = "\"#{sevenZip}\" a \"#{zip_name}\" \"#{all_files}\""
+            
+            # And execute it!
+			system command
+            
 		else
+        
+            # On the Mac use built-in ZIP function
 			system "zip -r #{zip_name} #{files.join(' ')}"
+            
 		end
+        
 	end
+    
 end
